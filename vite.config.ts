9aa37e8b108170c452538086cd162d2645ee0c93
Angélica Rodrigues Gonçalves <a.r.g.eng.soft@gmail.com>
2026-06-24 21:02:08 -0300

    Adicionar Nitro plugin para deploy na Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,14 @@ import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
+import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [
     devtools(),
     tailwindcss(),
-    viteTsConfigPaths(),
+    nitro(),
     tanstackStart(),
     viteReact(),
   ],
